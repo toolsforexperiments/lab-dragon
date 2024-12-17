78b06b40c9b50537ee1342413f18a281c772e2fe
marcosf2 <marcosf2@illinois.edu>
2024-12-16 20:38:19 -0600

    Added Libraries to Toolbar.jsx

--- dragon-scales/src/app/components/icons/DenLogo.jsx
+++ dragon-scales/src/app/components/icons/DenLogo.jsx
@@ -5,8 +5,8 @@ export default function DenLogo(props) {
             <g id="Group 187">
                 <path id="Vector 6"
                       d="M25.5 20.5H4L2 15L4 13.5L3 11L6 8.5L7 5.5L10.5 6L16.5 2L20 5.5L23.5 7V12L25.5 14V20.5Z"
-                      stroke="#059669" stroke-width="3" stroke-linejoin="round"/>
-                <path id="Vector 7" d="M10 16V20.5H18L19 16L16 13L11.5 13.5L10 16Z" fill="#059669" stroke="#059669"
+                      stroke="currentColor" stroke-width="3" stroke-linejoin="round" />
+                <path id="Vector 7" d="M10 16V20.5H18L19 16L16 13L11.5 13.5L10 16Z" fill="currentColor" stroke="currentColor"
                       stroke-width="2" stroke-linejoin="round"/>
             </g>
         </svg>
